6aea250b9e2d8d81a16f64caa2367e7b09424920
Heather Arbuckle <bucklehead@gmail.com>
2019-05-16 20:38:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+
+  for (var i = 1; i <= musicians.length; i++){
+    array.push(musicians[i] + " plays "+ instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(factArray) {
+  var num = factArray.length - 1;
+  while (num > 0) {
+     factArray[i] = factArray + "!!!";
+     num--;
+  }
+}
